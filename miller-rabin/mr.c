273f617a47150981ddc60a2219b315dfbd7e52e7
Robin Martinjak <rob@rmartinjak.de>
2012-03-01 23:30:05 +0100

    created mr.h

--- miller-rabin/mr.c
+++ miller-rabin/mr.c
@@ -1,3 +1,5 @@
+#include "mr.h"
+
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
@@ -5,10 +7,6 @@
 #define EVEN(n) (!(n & 1))
 
 #define WITNESSES 200
-unsigned long long shift_mod(unsigned long long a, unsigned int sh, unsigned long long mod);
-unsigned long long mult_mod(unsigned long long a, unsigned long long b, unsigned long long mod);
-unsigned long long pow_mod(unsigned long long base, unsigned long long exp, unsigned long long mod);
-unsigned long long randull(unsigned long long min, unsigned long long max);
 
 unsigned long long shift_mod(unsigned long long a, unsigned int sh, unsigned long long mod)
 {
---
+++ miller-rabin/mr.h
@@ -0,0 +1,4 @@
+unsigned long long shift_mod(unsigned long long a, unsigned int sh, unsigned long long mod);
+unsigned long long mult_mod(unsigned long long a, unsigned long long b, unsigned long long mod);
+unsigned long long pow_mod(unsigned long long base, unsigned long long exp, unsigned long long mod);
+unsigned long long randull(unsigned long long min, unsigned long long max);
