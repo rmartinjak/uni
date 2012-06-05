d7b18bf0996525b1a46536b07fa60245d99c86f4
Robin Martinjak <rob@rmartinjak.de>
2012-06-05 10:16:34 +0200

    worked on k-means a bit

--- k-means/kmeans.m
+++ k-means/kmeans.m
@@ -1,19 +1,50 @@
+# turn off warnings about automatic broadcasting
+warning("off", "Octave:broadcast")
+
+
+function [iter, ProtoMat] = kmeans (DataMat, ProtoMat)
+	ITER_MAX = 100;
+	EPSILON = 1e-4;
+
+	iter = 0;
+	Eold = 10000;
+	Enew = 1000;
+
+	while ++iter < ITER_MAX && abs(Eold - Enew) > EPSILON
+
+		# distance matrix
+		for i = 1:columns(DataMat)
+		for k = 1:columns(ProtoMat)
+		DistMat(k,i) = sumsq(DataMat(:,i) - ProtoMat(:,k));
+		end
+		end
+
+		AssignMat = zeros(columns(DataMat), columns(ProtoMat));
+
+		for i = 1:columns(DistMat)
+		[val, idx] = min(DistMat(:,i));
+		AssignMat(i, idx) = 1;
+		end
+
+		for i = 1:columns(ProtoMat)
+		ProtoMat(:,i) = sum(DataMat'.*AssignMat(:,i)) / sum(AssignMat(:,i));
+		end
+
+		Eold = Enew;
+		Enew = sum(sum(DistMat'.*AssignMat));
+	end
+endfunction
 
 # data vectors
-Dmat = [ randn(2, 100).*0.5, \
+DataMat = [ randn(2, 100).*0.5, \
 	randn(2, 100).*0.5 + [0; 1], \
 	randn(2, 100).*0.5 + [1; 0], \
-	randn(2, 100).*0.5 + [1; 1] ]
+	randn(2, 100).*0.5 + [1; 1] ];
 
-K = 4
+K = 4;
 
-Pmat = rand(2, K)
+ProtoMat = rand(2, K)
 
-
-# distance matrix
-for i = 1:size(v)(:,1)
-	for k = 1:size(p)(:,1)
-		D(i,k) = sumsq(v(i,:) - p(k,:))
-	end
-end
-
+[Iterations, ProtoMat] = kmeans(DataMat, ProtoMat);
+ProtoMat
+Iterations
