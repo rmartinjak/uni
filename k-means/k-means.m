d13c247184d26cc62bdbd1ca409dc715d92e04af
Robin Martinjak <rob@rmartinjak.de>
2012-06-08 15:02:59 +0200

    more work on k-means

--- k-means/k-means.m
+++ k-means/k-means.m
@@ -2,62 +2,66 @@
 warning('off', 'Octave:broadcast')
 
 
-function DataMat = GenDataMat()
+function Xmat = GenXmat()
 	dev = 0.25;
-	DataMat = [ randn(2, 100).*dev,  \
+	Xmat = [ randn(2, 100).*dev,  \
 		randn(2, 100).*dev + [0; 1], \
 		randn(2, 100).*dev + [1; 0], \
 		randn(2, 100).*dev + [1; 1] ];
 end
 
-function ProtoMat = RandProto(K, DataMat)
-	ProtoMat = rand(rows(DataMat), K);
+function Pmat = RandProto(K, Xmat)
+	Pmat = rand(rows(Xmat), K);
 end
 
-function ProtoMat = RandChoiceProto(K, DataMat)
-	ProtoMat = DataMat(:,randperm(columns(DataMat), K));
+function Pmat = RandChoiceProto(K, Xmat)
+	Pmat = Xmat(:,randperm(columns(Xmat), K));
 end
 
-function [Error, Iter, ProtoMat, Retries] = kmeans (DataMat, k, ProtoFun)
+function Hmat = mkAssign(Dmat)
+	Hmat = zeros(size(Dmat'));
+	for i = 1:columns(Dmat)
+		[val, idx] = min(Dmat(:,i));
+		Hmat(i, idx) = 1;
+	end
+end
+
+function Dmat = mkDist(Xmat, Pmat)
+	for i = 1:columns(Xmat)
+		for j = 1:columns(Pmat)
+			Dmat(j,i) = sumsq(Xmat(:,i) - Pmat(:,j));
+		end
+	end
+end
+
+function [Error, Iter, Pmat, Retries] = kmeans (Xmat, k, ProtoFun)
 	EPSILON = 1e-4;
 	ITER_MAX = 100;
 	E = zeros(ITER_MAX);
 	Iter = 0;
 	Retries = 0;
 
-	ProtoMat = ProtoFun(k, DataMat);
+	Pmat = ProtoFun(k, Xmat);
 
 	while ++Iter < ITER_MAX && ((Iter <= 2) || abs(E(Iter-2) - E(Iter-1)) > EPSILON)
 
-		% distance matrix
-		for i = 1:columns(DataMat)
-			for j = 1:columns(ProtoMat)
-				DistMat(j,i) = sumsq(DataMat(:,i) - ProtoMat(:,j));
-			end
-		end
-
-		AssignMat = zeros(columns(DataMat), columns(ProtoMat));
-		for i = 1:columns(DistMat)
-			[val, idx] = min(DistMat(:,i));
-			AssignMat(i, idx) = 1;
-		end
+		Dmat = mkDist(Xmat, Pmat);
+		Hmat = mkAssign(Dmat);
 
 		% check if there is one prototype without assigned data vectors
-		if !all(sum(AssignMat))
-			ProtoMat = ProtoFun(k, DataMat);
+		if !all(sum(Hmat))
+			Pmat = ProtoFun(k, Xmat);
 			Iter = 0;
 			Retries += 1;
 			continue
 		end
 
+		% calculate new prototypes
+		Pmat = Xmat * Hmat ./ sum(Hmat);
 
-		for i = 1:columns(ProtoMat)
-			ProtoMat(:,i) = sum(DataMat'.*AssignMat(:,i)) / sum(AssignMat(:,i));
+		E(Iter) = sum(sum(Dmat'.*Hmat));
+
 		end
-
-		E(Iter) = sum(sum(DistMat'.*AssignMat));
-
-	end
 
 	Error = E(Iter-1);
 end
@@ -72,8 +76,8 @@ function ex_a (iterations)
 	E = zeros(1, iterations);
 	Ret = 0;
 	for Iter = 1:iterations
-		DataMat = GenDataMat();
-		[Error, Iterations, ProtoMat, Retries] = kmeans(DataMat, K, @RandProto);
+		Xmat = GenXmat();
+		[Error, Iterations, Pmat, Retries] = kmeans(Xmat, K, @RandProto);
 		E(Iter) = Error;
 		Ret += Retries;
 	end
@@ -93,8 +97,8 @@ function ex_b (iterations)
 	E = zeros(1, iterations);
 	Ret = 0;
 	for Iter = 1:iterations
-		DataMat = GenDataMat();
-		[Error, Iterations, ProtoMat, Retries] = kmeans(DataMat, K, @RandChoiceProto);
+		Xmat = GenXmat();
+		[Error, Iterations, Pmat, Retries] = kmeans(Xmat, K, @RandChoiceProto);
 		E(Iter) = Error;
 		Ret += Retries;
 	end
@@ -113,12 +117,10 @@ function ex_c (iterations)
 	for K = 2:columns(E)
 		E(K) = Inf;
 		for i = 1:iterations
-			[Error, Iterations, ProtoMat] = kmeans(DataMat, K, @RandChoiceProto);
+			[Error, Iterations, Pmat] = kmeans(Xmat, K, @RandChoiceProto);
 			E(K) = min(E(K), Error);
 		end
 	end
 	plot(E(2:end))
 end
 
-ex_a(20)
-ex_b(20)
