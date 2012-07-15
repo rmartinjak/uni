716b4b815575ae8f725ba5d51ec6f1e51d0f2361
Robin Martinjak <rob@rmartinjak.de>
2012-07-15 18:59:23 +0200

    pcp.py: use deque instead of list

--- misc/pcp.py
+++ misc/pcp.py
@@ -1,17 +1,19 @@
+from collections import deque
+
 def pcp(pairs):
-    queue = []
+    queue = deque()
     for v1, v2 in pairs:
-        queue.insert(0, (1, v1, v2))
+        queue.append((1, v1, v2))
 
     while (len(queue) > 0):
-        n, w1, w2 = queue.pop()
+        n, w1, w2 = queue.popleft()
         for v1, v2 in pairs:
             s1 = w1 + v1
             s2 = w2 + v2
             if (s1 == s2):
                 return (n+1, s1)
             elif s1.startswith(s2) or s2.startswith(s1):
-                queue.insert(0, (n+1, s1, s2))
+                queue.append((n+1, s1, s2))
 
 if __name__ == '__main__':
     pairs = [ ("001", "0"), ("01", "011"), ("01", "101"), ("10", "001") ]
